f3e97978e40e33adc71627a911182ddbd8bc5df6
Kayam Uddin <email_address>
2024-08-15 12:33:25 +1000

    update feed definition

--- obj/Debug/netcoreapp3.1/AzureDevOpsWithBicep.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/AzureDevOpsWithBicep.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("azure-devOps-with-bicep")]
+[assembly: System.Reflection.AssemblyProductAttribute("Azure.DevOps.With.Bicep")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AzureDevOpsWithBicep")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
