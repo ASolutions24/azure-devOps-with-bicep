b51d69b8f5e4b30298ab027f5e04a34906e112bd
Kayam Uddin <email_address>
2024-08-15 11:32:05 +1000

    updated

--- scripts/Bicep-Deployment-Plan.ps1
+++
@@ -1,8 +0,0 @@
-
-copy-item "$env:System_ArtifactsDirectory/git-modules" "$env:System_ArtifactsDirectory/a/git-modules" -recurse #this is required to use same relative path for module reference in modules.bicep
-$TemplateParameterFile = "$env:System_ArtifactsDirectory"+ "/content/bicep-templates/main.parameters.json"
-$TemplateFile = "$env:System_ArtifactsDirectory"+ "/content/bicep-templates/main.bicep"
-
-$ResourceGroupName = "RG-Bicep-DevOps-Demo"
-
-New-AzResourceGroupDeployment -TemplateFile $TemplateFile -TemplateParameterFile $TemplateParameterFile -ResourceGroupName $ResourceGroupName -Whatif
